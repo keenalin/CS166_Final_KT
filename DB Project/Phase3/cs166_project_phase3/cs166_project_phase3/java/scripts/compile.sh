719009543b3d1d5fef6929cbf118b4eb8aa288bf
tcast054 <tcast054@ucr.edu>
2025-05-29 17:49:55 -0700

    Fix to allow us to run program from template "source compile.sh"

--- DB Project/Phase3/cs166_project_phase3/cs166_project_phase3/java/scripts/compile.sh
+++ DB Project/Phase3/cs166_project_phase3/cs166_project_phase3/java/scripts/compile.sh
@@ -3,9 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
 # compile the java program
-javac -d $DIR/../classes $DIR/../src/AirlineManagement.java
+javac -d "$DIR/../classes" "$DIR/../src/AirlineManagement.java"
 
-#run the java program
-#Use your database name, port number and login
-java -cp $DIR/../classes:$DIR/../lib/pg73jdbc3.jar AirlineManagement $USER"_project_phase_3_DB" $PGPORT $USER
+# run the java program
+# Use your database name, port number and login
+java -cp "$DIR/../classes:$DIR/../lib/pg73jdbc3.jar" AirlineManagement "${USER}_project_phase_3_DB" "$PGPORT" "$USER"
 
